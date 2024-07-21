6d1b3566ca327b29a82acf154532c4578d0b45b1
ivanalonsom <ivan7yt@gmail.com>
2024-07-21 14:07:56 +0200

    Calculadora en python v0.2 . Visual hecho.

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -2,11 +2,72 @@ from tkinter import *
 
 raiz = Tk()
 
-miFrame = Frame(raiz, width=1200, height=600)
+miFrame = Frame(raiz)
 miFrame.pack()
 
+# ------------ PANTALLA ------------
 
+pantalla = Entry(miFrame)
+pantalla.grid(row=1, column=1, padx=10, pady=10, columnspan=4)
+# Sin columnspan sale mal porque toma el elemento "pantalla" como columna 1 y este hace a esa columna mucho
+# más ancha que la 2, 3 y 4. Tenemos que decirle que "pantalla" ocupe las 4 columnas
+pantalla.config(bg="black", fg="#03f943", justify="right")
 
+# ------------ FILA 1 ------------
+
+boton7 = Button(miFrame, text="7", width=3)
+boton7.grid(row=2, column=1)
+
+boton8 = Button(miFrame, text="8", width=3)
+boton8.grid(row=2, column=2)
+
+boton9 = Button(miFrame, text="9", width=3)
+boton9.grid(row=2, column=3)
+
+botonDiv = Button(miFrame, text="/", width=3)
+botonDiv.grid(row=2, column=4)
+
+# ------------ FILA 2 ------------
+
+boton4 = Button(miFrame, text="4", width=3)
+boton4.grid(row=3, column=1)
+
+boton5 = Button(miFrame, text="5", width=3)
+boton5.grid(row=3, column=2)
+
+boton6 = Button(miFrame, text="6", width=3)
+boton6.grid(row=3, column=3)
+
+botonMult = Button(miFrame, text="X", width=3)
+botonMult.grid(row=3, column=4)
+
+# ------------ FILA 3 ------------
+
+boton1 = Button(miFrame, text="1", width=3)
+boton1.grid(row=4, column=1)
+
+boton2 = Button(miFrame, text="2", width=3)
+boton2.grid(row=4, column=2)
+
+boton3 = Button(miFrame, text="3", width=3)
+boton3.grid(row=4, column=3)
+
+botonRest = Button(miFrame, text="-", width=3)
+botonRest.grid(row=4, column=4)
+
+# ------------ FILA 4 ------------
+
+boton0 = Button(miFrame, text="0", width=3)
+boton0.grid(row=5, column=1)
+
+boton_coma = Button(miFrame, text=".", width=3)
+boton_coma.grid(row=5, column=2)
+
+boton_igual = Button(miFrame, text="=", width=3)
+boton_igual.grid(row=5, column=3)
+
+botonSuma = Button(miFrame, text="+", width=3)
+botonSuma.grid(row=5, column=4)
 
 
 
